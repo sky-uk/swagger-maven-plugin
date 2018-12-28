ae65e1fe6d37d074895f4076ea5a1c629ff6294b
colomboalb <alberto.colombo@skytv.it>
2018-12-28 16:13:12 +0100

    deleted all files but the MOJO

--- src/main/java/com/github/kongchen/swagger/docgen/PropertyExampleMixIn.java
+++
@@ -1,9 +0,0 @@
-package com.github.kongchen.swagger.docgen;
-
-import com.fasterxml.jackson.annotation.JsonRawValue;
-
-abstract class PropertyExampleMixIn {
-    PropertyExampleMixIn() { }
-    
-    @JsonRawValue abstract Object getExample();
-}
